72f197c293adfc9b2408f52f04ea2026efda7a5f
mclitao <mclitao@126.com>
2021-03-05 08:36:26 +0800

    change

--- app/resources/service.py
+++ app/resources/service.py
@@ -5,6 +5,36 @@ from flask_jwt import jwt_required, current_identity
 import os
 import subprocess
 
+"""
+服务器脚本命令
+"""
+PS_EF_MD='ps -ef |grep \'java -cp\' |grep quickfix.examples.executor.MarketDataServer |wc -l'
+PS_EF_ME='ps -ef |grep \'java -cp\' |grep quickfix.examples.ordermatch.MatchingEngine |wc -l'
+
+Start_MD='nohup java -cp  /Users/mclitao/Project/9999-futures/ccme/marketdata/target/ccme-marketdata-2.2.0-standalone.jar quickfix.examples.executor.MarketDataServer'
+Stop_MD='ps -ef |grep ccme-marketdata-2.2.0-standalone |awk \'{print $2}\'| grep -v grep |xargs kill -15'
+_Stop_MD='ps -ef |grep ccme-marketdata-2.2.0-standalone |awk \'{print $2}\' |wc -l'
+
+Start_ME='nohup java -cp ~/Project/9999-futures/ccme/matchingengine/target/ccme-mathcingengine-2.2.0-standalone.jar  quickfix.examples.ordermatch.MatchingEngine'
+Stop_ME='ps -ef |grep ccme-mathcingengine-2.2.0-standalone |awk \'{print $2}\'| grep -v grep |xargs kill -15'
+_Stop_ME='ps -ef |grep ccme-mathcingengine-2.2.0-standalone |awk \'{print $2}\' |wc -l'
+
+
+"""
+MD服务端口
+ mclitao@TaodeMacBook-Pro  ~  lsof -i :9880
+COMMAND   PID    USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
+java    49321 mclitao   55u  IPv6 0x3eacc9edad364093      0t0  TCP *:9880 (LISTEN)
+
+
+ME服务端口
+ mclitao@TaodeMacBook-Pro  ~  lsof -i :8323
+COMMAND   PID    USER   FD   TYPE             DEVICE SIZE/OFF NODE NAME
+java    49290 mclitao   49u  IPv6 0x3eacc9edad365913      0t0  TCP *:8323 (LISTEN)
+
+"""
+
+
 
 #负责执行CLI命令,并返回结果
 def exec_cli(_cmd):
@@ -17,7 +47,10 @@ def exec_cli(_cmd):
         out = 'Error'
 
     return out
-
+#处理掉特殊字符
+def change_str(out):
+    _str_s=out.replace('\r','').replace('\n','').replace('\t','').replace(' ','')
+    return _str_s
 
 #列出服务
 class ListService(Resource):
@@ -25,69 +58,93 @@ class ListService(Resource):
     def get(self):
 
         # MD服务检查
-        mdout=exec_cli('ps -ef|grep ccme-mathcingengine-2.2.0-standalone.jar')
-        if "ccme-mathcingengine-2.2.0-standalone.jar" in mdout :
-           _mdStatus=1
-        else:
-           _mdStatus=0
-
-
+        mdout=exec_cli(PS_EF_MD)
+        try:
+            if int(mdout)>1 :
+                _mdStatus=1
+            else:
+                _mdStatus=0
+        except:
+                _mdStatus=0
 
         # ME服务检查
-        meout=exec_cli('ps -ef |grep Me')
-        if "ME" in meout :
-           _meStatus=1
-        else:
-           _meStatus=0
+        meout=exec_cli(PS_EF_ME)
+        # print(meout)
+        try:
+            if int(meout)>1 :
+                _meStatus=1
+            else:
+                _meStatus=0
+        except:
+                _meStatus=0
 
         # 返回2个服务的真实状态   
-        return jsonify({'md':_mdStatus,"ME":_meStatus})
+        return jsonify({'MD':_mdStatus,"ME":_meStatus})
         
-# 
+
 
 #启动MD
 class MDStartService(Resource):
     # @jwt_required()
+    """
+       java启动 必须已cp参数启动不然后面ps -ef 查不到这个进程
+    """
     def get(self):
-        #out=exec_cli('ls -aslh .')
-        out=exec_cli('java -cp ~/Project/9999-futures/ccme/matchingengine/target/ccme-mathcingengine-2.2.0-standalone.jar  quickfix.examples.ordermatch.MatchingEngine')
-        if "Error" in out or '错误' in out: 
-            _status='0'
-        else: 
-            _status="1"
+        #检查是否已经启动过
+        out=exec_cli(PS_EF_MD)
+        print(int(change_str(out)))
+
+        if int(change_str(out)) >1: _status='1'
+        else:
+            #启动目标MD服务
+            out=exec_cli(Start_MD)
+            if "Error" in out or '错误' in out: 
+                _status='0'
+            else: 
+                _status="1"
         return {"status" : _status , "output": out }
 
 #停止MD
 class MDStopService(Resource):
     # @jwt_required()
     def get(self):
-        out=exec_cli('ps -ef |grep JavaProgramName |awk '{print $2}'| grep -v grep |xargs kill -15')
-        if "Error" in out or '错误' in out: 
-            _status='0'
+        out=exec_cli(Stop_MD)
+        _out=exec_cli('ps -ef |grep ccme-marketdata-2.2.0-standalone |awk \'{print $2}\' |wc -l')
+        
+        if int(_out) ==1: 
+            _status='1'
         else: 
-            _status="1"
-        _status = "on"
+            _status="0"
+        
         return {"status" : _status}
 
 #启动ME
 class MEStartService(Resource):
     # @jwt_required()
     def get(self):
-        #out=exec_cli('ls -aslh .')
-        out=exec_cli('java -cp ~/Project/9999-futures/ccme/matchingengine/target/ccme-mathcingengine-2.2.0-standalone.jar  quickfix.examples.ordermatch.MatchingEngine')
-        if "Error" in out or '错误' in out: 
-            _status='0'
-        else: 
-            _status="1"
+        #检查是否已经启动过
+        out=exec_cli(PS_EF_ME)
+        print(change_str(out))
+
+        if int(change_str(out))>1: _status='1'
+        else:
+            out=exec_cli(Start_ME)
+        
+            if "Error" in out or '错误' in out: 
+                _status='0'
+            else: 
+                _status="1"
         return {"status" : _status , "output": out }
 
 #停止ME
 class MEStopService(Resource):
     def get(self):
-        out=exec_cli('ps -ef |grep JavaProgramName |awk '{print $2}'| grep -v grep |xargs kill -15')
-        if "Error" in out or '错误' in out: 
-            _status='0'
+        out=exec_cli(Stop_ME)
+        _out=exec_cli(_Stop_ME)
+        
+        if int(_out) ==1: 
+            _status='1'
         else: 
-            _status="1"
-        _status = "on"
+            _status="0"
+                
         return {"status" : _status}
